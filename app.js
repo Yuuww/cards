ff6f4d05b5b7f703df2608c9d08940c4d4f9c299
Aqua <paul.fabian.bruhn@gmail.com>
2022-11-30 16:24:49 +0100

    stuff

---
+++ app.js
@@ -0,0 +1,32 @@
+const qHiragana = [
+    ['あ', 'a'],
+    ['い', 'i'],
+    ['う', 'u'],
+    ['え', 'e'],
+    ['お', 'o']
+];
+const khiragana = [
+    ['か', 'ka'],
+    ['き', 'ki'],
+    ['く', 'ku'],
+    ['け', 'ke'],
+    ['こ', 'ko']
+];
+const sHiragana = [
+    ['さ', 'sa'],
+    ['し', 'shi'],
+    ['す', 'su'],
+    ['せ', 'se'],
+    ['そ', 'so']
+];
+const tHiragana = [
+    ['た', 'ta'],
+    ['ち', 'chi'],
+    ['つ', 'tsu'],
+    ['て', 'te'],
+    ['と', 'to']
+];
+
+function showCard() {
+
+}
